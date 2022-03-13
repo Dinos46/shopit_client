987d07dd68dc25b4dc5e2e01cc61f7fccdc64dde
unknown <dinos4620@gmail.com>
2022-03-13 15:58:48 +0200

    add firebase login

---
+++ src/constants/appConfig.ts
@@ -0,0 +1,29 @@
+export const appConfig = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return {
+        firebase: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_ID,
+          appId: process.env.APP_ID,
+        },
+        baseUrl: process.env.APP_BASE_URL,
+      }
+
+    default:
+      return {
+        firebase: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_ID,
+          appId: process.env.APP_ID,
+        },
+        baseUrl: process.env.APP_BASE_URL,
+      }
+  }
+}
