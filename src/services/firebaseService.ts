5ca8913a927823427b8fab12dfd4326e07c93c0f
unknown <dinos4620@gmail.com>
2022-03-13 15:54:54 +0200

    add firebase login

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -2,6 +2,18 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { appConfig } from '../constants/appConfig'
 
-const app = initializeApp(appConfig().firebase)
+const firebaseConfig =
+  process.env.NODE_ENV === 'production'
+    ? {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_ID,
+        appId: process.env.APP_ID,
+      }
+    : appConfig().firebase
+
+const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
