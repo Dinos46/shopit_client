7497f499555b8540fb6e607b8b710501a8c4b11d
unknown <dinos4620@gmail.com>
2022-06-02 23:10:34 +0300

    hope its fix

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -10,14 +10,5 @@ const prod = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
 }
 
-// const prod = {
-//   apiKey: 'AIzaSyAeIyEmy0Run--3trdXiLH4gzhZLi2Q8ww',
-//   authDomain: 'shopit-fake-store-7cd6e.firebaseapp.com',
-//   projectId: 'shopit-fake-store-7cd6e',
-//   storageBucket: 'shopit-fake-store-7cd6e.appspot.com',
-//   messagingSenderId: '960824443656',
-//   appId: '1:960824443656:web:e1deb2fd2f2f02e0ae62bd',
-// }
-
 !getApps().length ? initializeApp(prod) : getApp()
 export const auth = getAuth()
