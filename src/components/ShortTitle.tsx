d6f125c85b3f5713935a3ea582aa0106f015c213
unknown <dinos4620@gmail.com>
2022-06-09 23:00:48 +0300

    card items style

---
+++ src/components/ShortTitle.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+type Props = {
+  title: string
+}
+
+const ShortTitle: React.FC<Props> = ({ title }) => {
+  const [isShown, setIsShown] = useState(false)
+
+  return (
+    <div className="self-end border-t-2 border-gray-200">
+      <h4 className="  font-pop  text-bl">
+        {title.slice(0, 19)}
+        {isShown && title.slice(19, title.length)}
+      </h4>
+
+      {!isShown && (
+        <span
+          className="cursor-pointer font-pop "
+          onClick={() => setIsShown(!isShown)}
+        >
+          show more...
+        </span>
+      )}
+      {isShown && (
+        <span
+          className="cursor-pointer font-pop "
+          onClick={() => setIsShown(!isShown)}
+        >
+          show less...
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default ShortTitle
